c8c0102faa150c8c5e01e4645a63229d614986a3
jhean <brizadaojhean@gmail.com>
2023-12-23 18:52:24 -0300

    :lipstick:	feat: melhorando a responsividade no header

--- src/components/layout/header/header-travelin.tsx
+++ src/components/layout/header/header-travelin.tsx
@@ -2,8 +2,10 @@ import NavTravelin from '../nav/nav-travelin'
 
 function HeaderTravelin() {
   return (
-    <header className="tw-container tw-flex tw-h-20 tw-items-center tw-bg-gradient-to-r tw-from-orange-500 tw-via-orange-600  tw-to-orange-700 tw-shadow-md">
-      <NavTravelin />
+    <header className="tw-bg-gradient-to-r tw-from-orange-500 tw-via-orange-600  tw-to-orange-700 tw-shadow-md">
+      <div className="tw-container tw-flex tw-h-20 tw-items-center ">
+        <NavTravelin />
+      </div>
     </header>
   )
 }
