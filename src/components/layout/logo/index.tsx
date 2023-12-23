dd57f983734c93723ab0fb6e9627682f756cd7ee
jhean <brizadaojhean@gmail.com>
2023-12-23 18:54:29 -0300

    :lipstick:	feat: melhorando a responsividade da logo

--- src/components/layout/logo/index.tsx
+++ src/components/layout/logo/index.tsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <Link
       href="/"
-      className="tw-font-serif tw-text-xl tw-font-bold tw-capitalize tw-text-orange-100 md:tw-text-3xl lg:tw-text-4xl"
+      className="tw-font-serif tw-text-3xl tw-font-bold tw-capitalize tw-text-orange-100 lg:tw-text-4xl"
     >
       Travelin
     </Link>
