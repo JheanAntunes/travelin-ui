b85095490ba136362b99cac6ca88f57c283efb5d
jhean <brizadaojhean@gmail.com>
2023-12-22 23:36:21 -0300

    :sparkles:	feat: header-travelin

---
+++ src/components/layout/header/header-travelin.tsx
@@ -0,0 +1,11 @@
+import NavTravelin from '../nav/nav-travelin'
+
+function HeaderTravelin() {
+  return (
+    <header className="tw-container tw-flex tw-h-20 tw-items-center tw-bg-gradient-to-r tw-from-orange-500 tw-via-orange-600  tw-to-orange-700 tw-shadow-md">
+      <NavTravelin />
+    </header>
+  )
+}
+
+export default HeaderTravelin
